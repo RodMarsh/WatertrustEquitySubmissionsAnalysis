cf2d77cc62cf534a5d5755477ba326043f6888c3
Sam Hames <sam@hames.id.au>
2024-01-30 11:24:20 +1000

    Add new inquiries/handle new exciting edge cases

--- inquiries/prepare_inquries.py
+++ inquiries/prepare_inquries.py
@@ -5,22 +5,12 @@ This script downloads all available files (if not already downloaded), then
 extracts the text from each file and saves it with appropriate metadata into
 a relational SQLite database.
 
-The moral foundations calculation is based on the following paper and
-scoring implementation:
+pip install requests pymupdf pytesseract
 
-Hopp, F.R., Fisher, J.T., Cornell, D. et al. The extended Moral Foundations
-Dictionary (eMFD): Development and applications of a crowd-sourced approach
-to extracting moral intuitions from text. Behav Res 53, 232–246
-(2021). https://doi.org/10.3758/s13428-020-01433-0
+You will also need to install the tesseract OCR library - how to install
+this depends on your OS. On Ubuntu the installation is:
 
-https://github.com/medianeuroscience/emfdscore
-
-Not that the EMFD library doesn't have fully specified dependencies, you will
-need to run the following first before anything will work:
-
-pip install spacy scikit-learn requests pymupdf pytesseract
-python -m spacy download en_core_web_sm
-pip install git+https://github.com/medianeuroscience/emfdscore.git
+sudo apt install tesseract-ocr
 
 """
 
@@ -33,7 +23,6 @@ import re
 import sqlite3
 import time
 
-from emfdscore import scoring
 import fitz
 from PIL import Image
 import pytesseract
@@ -44,72 +33,6 @@ try:
     os.mkdir("submissions")
 except FileExistsError:
     pass
-
-
-specific_inquiries = set(sys.argv[1:])
-if specific_inquiries:
-    print(f"Running for specific inquiries: {specific_inquiries}")
-
-db = sqlite3.connect("inquiries.db", isolation_level=None)
-
-db.executescript(
-    """
-    drop table if exists inquiry;
-    create table inquiry (
-        inquiry_shortname text primary key,
-        name
-    );
-
-    drop table if exists submission;
-    create table submission (
-        inquiry_shortname references inquiry,
-        submission_id,
-        url,
-        filepath,
-        submitter,
-        submitter_normalised,
-        text,
-        -- The following are the constructed categories for
-        -- the submitter, and are derived by manual labelling.
-        -- This is also available in the submission_label table for
-        -- convenience.
-        environmental integer,
-        regional integer,
-        consumptive integer,
-        research integer,
-        firstnations integer,
-        resourcemanagers integer,
-        government integer,
-        commercialnon integer,
-        notcategorisable integer,
-        primary key (inquiry_shortname, submission_id)
-    );
-
-    drop table if exists submission_label;
-    create table submission_label (
-        inquiry_shortname references inquiry,
-        submission_id,
-        label text,
-        priority integer check (priority between 1 and 3),
-        primary key (inquiry_shortname, submission_id, priority),
-        foreign key (inquiry_shortname, submission_id) references submission
-    );
-
-    drop table if exists submission_emfd_score;
-    create table submission_emfd_score (
-        inquiry_shortname references inquiry,
-        submission_id,
-        score_type text,
-        score float,
-        primary key (inquiry_shortname, submission_id, score_type),
-        foreign key (inquiry_shortname, submission_id) references submission
-    );
-
-    pragma journal_mode=WAL;
-    pragma foreign_keys=1;
-
-    """
-)
 
 
 def extract_text_from_file(
@@ -168,8 +91,21 @@ def extract_text_from_file(
 
                     seen_images.add(image[0])
                     pixmap = fitz.Pixmap(submission, image[0])
+                    try:
+                        im_data = pixmap.tobytes("png")
+                    except Exception:
+                        try:
+                            im_data = pixmap.pil_tobytes("png")
+                        except Exception:
+                            # The edge case here should be the CMYK images, which
+                            # are only used in certain prepared for print
+                            # submissions.
+                            print(f"skipping unhandled pixmap {pixmap}")
+                            continue
+
                     ocr_text = pytesseract.image_to_string(
-                        Image.open(io.BytesIO(pixmap.tobytes("png"))),
+                        # Handle indexed images conversion to png
+                        Image.open(io.BytesIO(im_data)),
                         lang="eng",
                         # This mode is page auto segmentation with orientation
                         # and script detection - the default is page auto
@@ -275,152 +211,196 @@ def normalise_submitter(submitter):
     return submitter
 
 
-# Note this is inferred from the emfd code - as written it errors with current
-# versions of spacy, so there might have been some drift or change over
-# time.
-nlp = spacy.load("en_core_web_sm", disable=["ner", "parser"])
-# Can safely increase the default to accomodate the longer submissions -
-# spacy warns about significant memory usage in the ner and parser components
-# which we've already disabled.
-nlp.max_length = 2000000
+all_args = set(sys.argv[1:])
+
+if "apply_labels_only" in all_args:
+    apply_labels_only = True
+else:
+    specific_inquiries = all_args
+    print(f"Running for specific inquiries: {specific_inquiries}")
+
+db = sqlite3.connect("inquiries.db", isolation_level=None)
 
 
-def score_emfd(text):
-    doc = nlp(text)
-    tokens = scoring.tokenizer(doc)
-    return scoring.score_emfd_all_sent(tokens)
+if not apply_labels_only:
+    db.executescript(
+        """
+        drop table if exists inquiry;
+        create table inquiry (
+            inquiry_shortname text primary key,
+            name
+        );
 
+        drop table if exists submission;
+        create table submission (
+            inquiry_shortname references inquiry,
+            submission_id,
+            url,
+            filepath,
+            submitter,
+            submitter_normalised,
+            text,
+            -- The following are the constructed categories for
+            -- the submitter, and are derived by manual labelling.
+            -- This is also available in the submission_label table for
+            -- convenience.
+            environmental integer,
+            regional integer,
+            consumptive integer,
+            research integer,
+            firstnations integer,
+            resourcemanagers integer,
+            government integer,
+            commercialnon integer,
+            notcategorisable integer,
+            primary key (inquiry_shortname, submission_id)
+        );
 
-session = requests.Session()
+        drop table if exists submission_emfd_score;
 
+        pragma journal_mode=WAL;
+        pragma foreign_keys=1;
 
-db.execute("begin")
+        """
+    )
 
-with open("inquiries.csv", "r") as inquiries_file, open(
-    "boilerplate.log", "w"
-) as boilerplate_log:
-    inquiries = csv.DictReader(inquiries_file, quoting=csv.QUOTE_ALL)
+    session = requests.Session()
 
-    for inquiry in inquiries:
-        shortname = inquiry["inquiry_shortname"]
+    db.execute("begin")
 
-        # Used for testing - normal runs won't touch this.
-        if specific_inquiries and shortname not in specific_inquiries:
-            continue
+    with open("inquiries.csv", "r") as inquiries_file, open(
+        "boilerplate.log", "w"
+    ) as boilerplate_log:
+        inquiries = csv.DictReader(inquiries_file, quoting=csv.QUOTE_ALL)
 
-        submissions_path = inquiry["submission_reference_file"]
+        for inquiry in inquiries:
+            shortname = inquiry["inquiry_shortname"]
 
-        boilerplate_form = inquiry["boilerplate_regex"]
-        if boilerplate_form:
-            print(f"Detecting boilerplate with '{boilerplate_form}'")
-            boilerplate_detector = re.compile(boilerplate_form)
-        else:
-            boilerplate_detector = None
+            # Used for testing - normal runs won't touch this.
+            if specific_inquiries and shortname not in specific_inquiries:
+                continue
 
-        # make sure the submission specific file exists.
-        try:
-            os.mkdir(os.path.join("submissions", shortname))
-        except FileExistsError:
-            pass
+            submissions_path = inquiry["submission_reference_file"]
 
-        print(f"Processing {shortname=}")
-        db.execute("insert into inquiry values(:inquiry_shortname, :name)", inquiry)
+            boilerplate_form = inquiry["boilerplate_regex"]
+            if boilerplate_form:
+                print(f"Detecting boilerplate with '{boilerplate_form}'")
+                boilerplate_detector = re.compile(boilerplate_form)
+            else:
+                boilerplate_detector = None
 
-        with open(submissions_path, "r") as submissions_header:
-            submissions = csv.DictReader(submissions_header, quoting=csv.QUOTE_ALL)
+            # make sure the submission specific file exists.
+            try:
+                os.mkdir(os.path.join("submissions", shortname))
+            except FileExistsError:
+                pass
 
-            for submission in submissions:
-                submission_id = submission["id"]
-                submission_format = submission["format"]
-                download_to = os.path.join(
-                    "submissions", shortname, f"{submission_id}.{submission_format}"
-                )
-                temp = download_to + ".temp"
+            print(f"Processing {shortname=}")
+            db.execute("insert into inquiry values(:inquiry_shortname, :name)", inquiry)
 
-                # Any submission with a URL is consider
-                public_submission = bool(submission["submission_url"])
+            with open(submissions_path, "r") as submissions_header:
+                submissions = csv.DictReader(submissions_header, quoting=csv.QUOTE_ALL)
 
-                # Download if a link is present and the file is not already present.
-                if (
-                    not os.path.exists(download_to)
-                    and public_submission
-                    and submission_format != "skip"
-                ):
-                    r = session.get(submission["submission_url"], allow_redirects=True)
-
-                    # Make sure to actually check the status code, not
-                    # just write a forbidden response to the output.
-                    r.raise_for_status()
-                    with open(temp, "wb") as temp_file:
-                        temp_file.write(r.content)
-
-                    os.rename(temp, download_to)
-                    # Wait at least 2 seconds, and on average 10 seconds with
-                    # some jitter.
-                    time.sleep(2 + random.random() * 16)
-
-                # Extract text from the submission and stuff in the database
-                if public_submission:
-                    print(submission)
-                    text, skipped = extract_text_from_file(
-                        download_to,
-                        submission_format,
-                        boilerplate_detector,
-                        int(submission["page_start"])
-                        if submission["page_start"]
-                        else 0,
-                        int(submission["page_end"])
-                        if submission["page_end"]
-                        else 2**32,
+                for submission in submissions:
+                    submission_id = submission["id"]
+                    submission_format = submission["format"]
+                    download_to = os.path.join(
+                        "submissions", shortname, f"{submission_id}.{submission_format}"
                     )
-                    boilerplate_log.writelines(skipped)
+                    temp = download_to + ".temp"
 
-                else:
-                    text = ""
+                    # Any submission with a URL is consider
+                    public_submission = bool(submission["submission_url"])
 
-                submission["inquiry_shortname"] = shortname
-                submission["text"] = text
-                submission["filepath"] = download_to
-                submission["submitter_normalised"] = normalise_submitter(
-                    submission["submitter"]
-                )
+                    # Download if a link is present and the file is not already present.
+                    if (
+                        not os.path.exists(download_to)
+                        and public_submission
+                        and submission_format != "skip"
+                    ):
+                        r = session.get(
+                            submission["submission_url"], allow_redirects=True
+                        )
 
-                db.execute(
-                    """
-                    insert into submission(
-                        inquiry_shortname,
-                        submission_id,
-                        url,
-                        filepath,
-                        submitter,
-                        submitter_normalised,
-                        text
-                    ) values (
-                        :inquiry_shortname,
-                        :id,
-                        :submission_url,
-                        :filepath,
-                        :submitter,
-                        :submitter_normalised,
-                        :text
+                        # Make sure to actually check the status code, not
+                        # just write a forbidden response to the output.
+                        r.raise_for_status()
+                        with open(temp, "wb") as temp_file:
+                            temp_file.write(r.content)
+
+                        os.rename(temp, download_to)
+                        # Wait at least 2 seconds, and on average 10 seconds with
+                        # some jitter.
+                        time.sleep(2 + random.random() * 16)
+
+                    # Extract text from the submission and stuff in the database
+                    if public_submission:
+                        print(submission)
+                        text, skipped = extract_text_from_file(
+                            download_to,
+                            submission_format,
+                            boilerplate_detector,
+                            int(submission["page_start"])
+                            if submission["page_start"]
+                            else 0,
+                            int(submission["page_end"])
+                            if submission["page_end"]
+                            else 2**32,
+                        )
+                        boilerplate_log.writelines(skipped)
+
+                    else:
+                        text = ""
+
+                    submission["inquiry_shortname"] = shortname
+                    submission["text"] = text
+                    submission["filepath"] = download_to
+                    submission["submitter_normalised"] = normalise_submitter(
+                        submission["submitter"]
                     )
-                    """,
-                    submission,
-                )
 
-                # Apply EMFD score:
-                scores = score_emfd(text)
-
-                for score_type, score in scores.items():
-                    submission["score_type"] = score_type
-                    submission["score"] = score
                     db.execute(
-                        "insert into submission_emfd_score values(:inquiry_shortname, :id, :score_type, :score)",
+                        """
+                        insert into submission(
+                            inquiry_shortname,
+                            submission_id,
+                            url,
+                            filepath,
+                            submitter,
+                            submitter_normalised,
+                            text
+                        ) values (
+                            :inquiry_shortname,
+                            :id,
+                            :submission_url,
+                            :filepath,
+                            :submitter,
+                            :submitter_normalised,
+                            :text
+                        )
+                        """,
                         submission,
                     )
+    db.execute("commit")
+
 
 # Merge in all of the current labels
+db.execute("begin")
+
 with open("submitter_labels.csv", "r") as f:
+    db.execute("drop table if exists submission_label")
+    db.execute(
+        """
+        create table submission_label (
+            inquiry_shortname references inquiry,
+            submission_id,
+            label text,
+            priority integer check (priority between 1 and 3),
+            primary key (inquiry_shortname, submission_id, priority),
+            foreign key (inquiry_shortname, submission_id) references submission
+        )
+        """
+    )
     reader = csv.DictReader(f)
     for row in reader:
         # Update the new table with long format labels
